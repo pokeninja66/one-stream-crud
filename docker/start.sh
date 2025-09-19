76efb4023cc6c8d87ed26ff00cc9058e089cd044
pokeninja66 <georgimalamov97@gmail.com>
2025-09-19 08:22:45 +0300

    updated port configuration

--- docker/start.sh
+++ docker/start.sh
@@ -4,5 +4,9 @@ set -e
 echo "Starting PHP-FPM..."
 php-fpm83 -D
 
+echo "Configuring NGINX for port ${PORT:-80}..."
+# Replace port in nginx config
+sed -i "s/listen 80;/listen ${PORT:-80};/" /etc/nginx/nginx.conf
+
 echo "Starting NGINX..."
 nginx -g "daemon off;"
