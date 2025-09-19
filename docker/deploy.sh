180ab98b138cb4550d5dc44f57347d8be33d60c5
pokeninja66 <georgimalamov97@gmail.com>
2025-09-19 08:59:29 +0300

    .

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -13,8 +13,8 @@ npm run build
 echo "Removing development dependencies..."
 npm prune --production
 
-echo "Generating application key..."
-php artisan key:generate --ansi || true
+# echo "Generating application key..."
+# php artisan key:generate --ansi || true
 
 echo "Caching config..."
 php artisan config:cache
