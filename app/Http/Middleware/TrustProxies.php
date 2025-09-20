6d41c3b9a5ecebcff7421fd936e9d6ec480ec40b
pokeninja66 <georgimalamov97@gmail.com>
2025-09-20 15:51:06 +0300

    Added a TrustProxies middware

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    // Trust all proxies for render.com. Just need it for the swagger UI docs to load.
+    protected $proxies = '*'; 
+
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
