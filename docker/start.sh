eab6a1ed294b13d97bcd6d58699a70ba2a81bcb2
pokeninja66 <georgimalamov97@gmail.com>
2025-09-20 18:07:25 +0300

    fix build

--- docker/start.sh
+++ docker/start.sh
@@ -3,25 +3,6 @@ set -e
 
 echo "Running Laravel setup..."
 
-# Create .env file with production configuration
-echo "Creating .env file..."
-cat > .env <<EOL
-APP_NAME=oneStreamCrud
-APP_ENV=production
-APP_KEY=base64:+/HoJ/aH9iqq/jkbx6e7CG3bIERt0XGPSQ4YZ7BgB9g=
-APP_DEBUG=false
-APP_URL=https://one-stream-crud.onrender.com
-
-DATABASE_URL=postgresql://one_stream_crud_user:lo4apazpytLxEdextlbCK0PQlcnMWE46@dpg-d36746be5dus73fqdma0-a/one_stream_crud
-DB_CONNECTION=pgsql
-DB_HOST=dpg-d36746be5dus73fqdma0-a
-DB_PORT=5432
-DB_DATABASE=one_stream_crud
-DB_USERNAME=one_stream_crud_user
-DB_PASSWORD=lo4apazpytLxEdextlbCK0PQlcnMWE46
-L5_SWAGGER_CONST_HOST=https://one-stream-crud.onrender.com
-L5_SWAGGER_GENERATE_ALWAYS=true
-EOL
 
 # Set production environment variables
 export APP_ENV=production
@@ -51,6 +32,7 @@ php artisan vendor:publish --provider="L5Swagger\L5SwaggerServiceProvider"
 
 # Generate API documentation
 echo "Generating API documentation..."
+php artisan l5-swagger:generate
 php artisan idoc:generate
 
 # Copy and configure Swagger UI static assets
