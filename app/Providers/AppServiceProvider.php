c1b97461e3a8b03ec403680bcb22d42be40f5f1a
pokeninja66 <georgimalamov97@gmail.com>
2025-09-20 12:06:34 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        // Force HTTPS in production
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        }
+        //     // Force HTTPS in production
+        //     if (env('APP_ENV') == 'production') {
+        //         $url->forceScheme('https');
+        //     }
+        // Always force HTTPS (for both production and development - need to remove this in dev later)
+        $url->forceScheme('https');
     }
 }
